30f71a6b76ca00c9253212d7694cf5ee4958f0fd
tonky0110 <tonky0110@gmail.com>
2019-12-15 21:34:10 +0900

    2.8 express Route분기

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import morgan from 'morgan';
-
+import { userRouter } from './router';
 const app = express();
 
 // middleware
@@ -12,5 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
+
+// router
+app.use('/user', userRouter);
 
 export default app;
