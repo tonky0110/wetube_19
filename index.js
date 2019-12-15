2a5e23b29f3ecc4ac72597a76a9344616f8e9210
tonky0110 <tonky0110@gmail.com>
2019-12-15 20:11:18 +0900

    2.4 url에 따른 handleMethod분기 처리.

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 
 const PORT = 4000;
+
 function handleListening() {
 	console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handleProfile(req, res) {
+	console.log(req);
+	res.send('You are on my profile.');
+}
+
+function handleHome(req, res) {
+	console.log(req);
+	res.send('Hello from home.');
+}
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
+
 app.listen(PORT, handleListening);
