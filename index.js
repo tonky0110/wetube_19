8a1bfb58aa7a2bd8ac66e6b774dd619d9d9e0a29
tonky0110 <tonky0110@gmail.com>
2019-12-15 20:38:04 +0900

    2.6.1 Middleware - 중간에 인터럽트해서 실행되는 function(logging, auth등의 작업들을 처리할 수 있다.)

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const handleProfile = (req, res) => res.send('You are on my profile.');
 
 const handleHome = (req, res) => res.send('Hello from home.');
 
-app.get('/', handleHome);
+// middleware
+const betweenHome = (req, res, next) => {
+	console.log('Between');
+	next();
+};
+
+app.get('/', betweenHome, handleHome);
 app.get('/profile', handleProfile);
 
 app.listen(PORT, handleListening);
