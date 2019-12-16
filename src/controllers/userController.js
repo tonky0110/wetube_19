8784affeca32d44f34dc1e3fc1e66a0c9b9f33a2
tonky0110 <tonky0110@gmail.com>
2019-12-16 22:19:32 +0900

    2.17 send props to each pages

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,7 @@
-export const join = (req, res) => res.send('Join');
-export const login = (req, res) => res.send('Login');
-export const logout = (req, res) => res.send('Logout');
-
-export const users = (req, res) => res.send('users');
-export const userDetail = (req, res) => res.send('userDetail');
-export const editProfile = (req, res) => res.send('editProfile');
-export const changePassword = (req, res) => res.send('changePassword');
+export const join = (req, res) => res.render('Join', { pageTitle: 'Join' });
+export const login = (req, res) => res.render('Login', { pageTitle: 'Login' });
+export const logout = (req, res) => res.render('Logout', { pageTitle: 'Logout' });
+export const users = (req, res) => res.render('users', { pageTitle: 'Users' });
+export const userDetail = (req, res) => res.render('userDetail', { pageTitle: 'User Detail' });
+export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'Edit Profile' });
+export const changePassword = (req, res) => res.render('changePassword', { pageTitle: 'Change Password' });
