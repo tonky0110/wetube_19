3e73caa6421ee7d8cd671c7217ba8dbe2bd11998
tonky0110 <tonky0110@gmail.com>
2019-12-17 22:02:32 +0900

    2.20 Edit Profile and link change Password

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,10 @@ import routes from '../routes';
 import { videos, upload, videoDetail, editVideo, deleteVideo } from '../controllers/videoController';
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
+// videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
+
 export default videoRouter;
