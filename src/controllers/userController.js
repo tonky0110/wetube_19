352116caecd4f59f348088dc64fa0e870c42727f
tonky0110 <tonky0110@gmail.com>
2019-12-19 22:25:07 +0900

    2.23 join password만 비교하고 다르면 400, 같으면 home으로 리다이렉트

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,22 @@
-export const join = (req, res) => res.render('Join', { pageTitle: 'Join' });
+import routes from '../routes';
+
+export const getJoin = (req, res) => {
+	res.render('Join', { pageTitle: 'Join' });
+};
+
+export const postJoin = (req, res) => {
+	console.log(req.body);
+	const { body: { name, email, password, password2 } } = req;
+	if (password !== password2) {
+		res.status(400);
+		res.render('Join', { pageTitle: 'Join' });
+	} else {
+		// To Do: Register User
+		// To Do: Log user in
+		res.redirect(routes.home);
+	}
+};
+
 export const login = (req, res) => res.render('Login', { pageTitle: 'Login' });
 export const logout = (req, res) => res.render('Logout', { pageTitle: 'Logout' });
 export const users = (req, res) => res.render('users', { pageTitle: 'Users' });
