eb29c94a6307b5f73d4abe33e6855d91facf6b8b
tonky0110 <tonky0110@gmail.com>
2019-12-21 00:11:51 +0900

    3.2 configuring Dot Env .env파일을 통해서 숨겨야하는 중요 config정보를 보호 가장 상단에서 import dotenv from "dotenv";로 추가하고, dotenv.config()를 호출해서 불러옴. 이후부터 process.env.KEY_VALUE로 필수 값을 호출.

--- src/db.js
+++ src/db.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect('mongodb://localhost:27017/we-tube', {
+mongoose.connect(process.env.MONGO_URL, {
 	useNewUrlParser: true,
 	useFindAndModify: false
 });
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log('✅ Connected to DB');
+const handleOpen = () => console.log(`✅ Connected to DB: ${process.env.MONGO_URL}`);
 const handleError = () => console.log(`❌ Error on DB Connection: ${error}`);
 
 db.once('open', handleOpen);
