b237b74e5edc98a3de06fac4b395601424b7b9a5
tonky0110 <tonky0110@gmail.com>
2019-12-21 22:25:39 +0900

    4.1 webpack.config part(1/2) webpack은 아래에서 위로 실행됨. scss --> postcss --> css 순으로 변경되어서 실행. 윈도우에서는 cross-env를 설치 후 "cross-env WEBPACK_ENV=***"으로 설정."

--- src/db.js
+++ src/db.js
@@ -1,16 +1,18 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {
-	useNewUrlParser: true,
-	useFindAndModify: false
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log(`✅ Connected to DB: ${process.env.MONGO_URL}`);
+const handleOpen = () =>
+  console.log(`✅ Connected to DB: ${process.env.MONGO_URL}`);
 const handleError = () => console.log(`❌ Error on DB Connection: ${error}`);
 
-db.once('open', handleOpen);
-db.on('error', handleError);
+db.once("open", handleOpen);
+db.on("error", handleError);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require("path");
+const ExtractCSS = require("extract-text-webpack-plugin");
 
-const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "src", "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 const config = {
   entry: ENTRY_FILE,
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractCSS.extract([
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ])
+      }
+    ]
+  },
   output: {
     path: OUTPUT_DIR,
     filename: "[name].[format]"
