0aaec590762b09bdcf15c27db5530ca1002dc572
tonky0110 <tonky0110@gmail.com>
2019-12-22 20:59:30 +0900

    6.1 local authentication with pasport passport에서 미리 생성해 놓은 id/pwd 인증 function을 사용하기 위하여 passport.js를 생성 및 connect

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,7 +3,7 @@ const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
-const ENTRY_FILE = path.resolve(__dirname, "src", "assets", "js", "main.js");
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 const config = {
   entry: ["@babel/polyfill", ENTRY_FILE],
