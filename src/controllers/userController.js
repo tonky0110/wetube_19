3e700326f2d3dc61881e66565c52d80c2e5aa9cd
tonky0110 <tonky0110@gmail.com>
2019-12-25 19:52:11 +0900

    6.6 passport-github

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,6 +39,10 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
 export const logout = (req, res) => {
   // To Do: Process Log out
   req.logout();
