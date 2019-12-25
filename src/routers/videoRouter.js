57707f1fe00e1f36932a1efb15c38fe40fc6ba8f
tonky0110 <tonky0110@gmail.com>
2019-12-25 14:31:17 +0900

    6.5.3 Routert수정 globalRouter: 로그인한 사용자가 join, login등의 로그인하지 않을때 사용하는 url을 접근제어. userRouter: 동일. videoRouter: 로그인하지 않은 사용자가 upload, editVideo, deleteVideo를 접근 제어.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,27 +1,27 @@
-import express from 'express';
-import { uploadVideo } from '../middlewares';
-import routes from '../routes';
+import express from "express";
+import { uploadVideo, onlyPrivate } from "../middlewares";
+import routes from "../routes";
 import {
-	deleteVideo,
-	getEditVideo,
-	getUpload,
-	videoDetail,
-	postEditVideo,
-	postUpload
-} from '../controllers/videoController';
+  deleteVideo,
+  getEditVideo,
+  getUpload,
+  videoDetail,
+  postEditVideo,
+  postUpload
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 // Upload
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
 // Edit Video
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 
 // Delete Video
-videoRouter.get(routes.deleteVideo(), deleteVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 
 // Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
